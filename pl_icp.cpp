fb378775e7eb99e4de0fa949adc45369290aae7d
Xiayue <69243170+bob292@users.noreply.github.com>
2023-06-29 15:11:15 +0800

    Update pl_icp.cpp
    Using plc icp to perform scan matching

--- pl_icp.cpp
+++ pl_icp.cpp
@@ -8,7 +8,10 @@
 #include <pcl/console/parse.h>
 #include <pcl/common/transforms.h>	//	pcl::transformPointCloud 用到这个头文件
 #include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/cloud_viewer.h>
 #include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/registration/icp.h>
+#include <pcl/point_types.h>
 
 #include <json/json.h>
 #include <json/value.h>
@@ -20,7 +23,7 @@
 using namespace Eigen;
 using namespace std;
 
-#define MAX_DIS 0.25
+#define MAX_DIS 5
 
 // created for ceres
 struct Pl_ICP 
@@ -42,14 +45,14 @@ struct Pl_ICP
             
             //q_last_curr.normalize();
             Eigen::Matrix<T,3,1> t_last_curr{t[0],t[1],t[2]};
-            Eigen::Matrix<T, 3, 1> lp;
-            lp = q_last_curr *cp + t_last_curr;
+            Eigen::Matrix<T,3,1> lp;
+            lp = q_last_curr *cp + t_last_curr;//quaternion overload * (pvp^-1)
 
             Eigen::Matrix<T,3,1> AB = lpb  - lpa;
             Eigen::Matrix<T,3,1> AC = lp -lpa ;
             Eigen::Matrix<T,3,1> abCrossac = AB.cross(AC);
-             residual[0] = (abCrossac.norm())/ (AB.norm()); //一个点的cost function:点线距离
-            
+            residual[0] = (abCrossac.norm())/ (AB.norm()); //一个点的cost function:点线距离
+            //residual[0] = AC.norm();
             return true;
         }
 
@@ -113,7 +116,6 @@ void loadData(const string &path,
                         if(root[i]["gt"][j]["label"]==0)
                         {
                             coordi0.push_back({root[i]["gt"][j]["x"][k].asDouble(),root[i]["gt"][j]["y"][k].asDouble()});
-
                         }
 
                         if(root[i]["gt"][j]["label"]==1)
@@ -149,7 +151,7 @@ void loadData(const string &path,
             for (auto& point: pc_cloud){
                 point.x = coordi1[pcp][0];
                 point.y = coordi1[pcp][1];
-                point.z = 0;
+                point.z = MAX_DIS * 2;
                 point.intensity = 122;
                 pcp++;
             }
@@ -162,7 +164,7 @@ void loadData(const string &path,
             for (auto& point: rb_cloud){
                 point.x = coordi2[rbp][0];
                 point.y = coordi2[rbp][1];
-                point.z = 0;
+                point.z = MAX_DIS *4;
                 point.intensity = 255;
                 rbp++;
             }
@@ -186,25 +188,17 @@ void runPl_ICP( Eigen::Quaterniond &pred_q,
                 pcl::PointCloud<pcl::PointXYZI> &currentxyzi_cloud){
                 cout<<"Start PL-ICP"<<endl;
                 pcl::PointCloud<pcl::PointXYZI>::Ptr transformedxyzi_cloud(new pcl::PointCloud<pcl::PointXYZI>());
-                pcl::KdTreeFLANN<pcl::PointXYZ> pcl_kdtree_ptr_;
-                //将local_map的点云数据输入到kdtree当中，方便后续查找
-                /*for(int i = 0; i < pre_cloud.size(); ++i){
-                        pcl::transformPointCloud(
-                            pre_cloud.at(i).frame_, 
-                            *transformed_cloud_ptr, 
-                            pre_cloud.at(i).pose
-                        );
-                        *map_ptr_ += *transformed_cloud_ptr;
-                    }*/
+
                 cout<<"Transform point cloud"<<endl;
-                pcl::transformPointCloud(pre_cloud,*transformedxyzi_cloud,T_pc.matrix());
-                /*for(auto point : currentxyzi_cloud){
+                pcl::transformPointCloud(pre_cloud,*transformedxyzi_cloud, T_pc.matrix());
+                //*transformedxyzi_cloud += pre_cloud;
+                /*for(auto point : *transformedxyzi_cloud){
                     cout<<point.x<<" "<<point.y<<" "<<point.z<<" "<<point.intensity<<endl;
                 }*/
                 /************************************************************************
                  try xyz point
                 ***********************************************************************/
-                cout<<"start transforming xyz point"<<endl;
+                cout<<"start transforming transformed cloud to xyz point"<<endl;
                 pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>());
                 transformed_cloud->width = transformedxyzi_cloud->width;
                 transformed_cloud->height = 1;
@@ -215,7 +209,8 @@ void runPl_ICP( Eigen::Quaterniond &pred_q,
                     transformed_cloud->points[i].y = transformedxyzi_cloud->points[i].y;
                     transformed_cloud->points[i].z = transformedxyzi_cloud->points[i].z;
                 }
-                cout<<"transformed xyz completed"<<endl;
+
+                cout<<"start transforming current cloud to xyz point"<<endl;
                 pcl::PointCloud<pcl::PointXYZ>::Ptr current_cloud(new pcl::PointCloud<pcl::PointXYZ>());
                 current_cloud->width = currentxyzi_cloud.width;
                 current_cloud->height = 1;
@@ -226,10 +221,11 @@ void runPl_ICP( Eigen::Quaterniond &pred_q,
                     current_cloud->points[j].y = currentxyzi_cloud.points[j].y;
                     current_cloud->points[j].z = currentxyzi_cloud.points[j].z;
                 }
-                
+
+                //将local_map的点云数据输入到kdtree当中，方便后续查找
+                pcl::KdTreeFLANN<pcl::PointXYZ> pcl_kdtree_ptr_;
                 pcl_kdtree_ptr_.setInputCloud(transformed_cloud);
                 //pcl_kdtree_ptr_.setInputCloud(current_cloud);
-                cout<<"setInputCloud no segmentation error"<<endl;
                 
                 //将当前帧得到的二维激光点云每个点查找之前输入kdtree中的local_map最近的两个点云进行残差块的输入
                 const int point_num = current_cloud->points.size();
@@ -249,11 +245,9 @@ void runPl_ICP( Eigen::Quaterniond &pred_q,
                 double t[3] = {0.0, 0.0, 0.0};
                 } param_;
                 ceres::Problem problem_;
-                //后续判断加入的优化变量纬度是否准确
+                //configs
+                {//后续判断加入的优化变量纬度是否准确
                 config_.q_parameterization_ptr = new ceres::EigenQuaternionParameterization();
-                //损失函数
-                
-            
                 //验证是否优化变量的纬度是否正确
                 problem_.AddParameterBlock(param_.q, 4, config_.q_parameterization_ptr);
                 problem_.AddParameterBlock(param_.t, 3);
@@ -263,14 +257,15 @@ void runPl_ICP( Eigen::Quaterniond &pred_q,
                 // 
                 
                 config_.options.minimizer_type = ceres::TRUST_REGION;
-                config_.options.trust_region_strategy_type = ceres::DOGLEG;
+                config_.options.trust_region_strategy_type = ceres::LEVENBERG_MARQUARDT;//ceres::DOGLEG;
+                config_.options.use_nonmonotonic_steps = true;//new try
                 config_.options.dogleg_type = ceres::SUBSPACE_DOGLEG;
                 config_.options.num_threads = 8;
-                config_.options.max_num_iterations = 100;
+                config_.options.max_num_iterations = 500;
                 config_.options.minimizer_progress_to_stdout = false;
-                config_.options.max_solver_time_in_seconds = 0.10;
+                config_.options.max_solver_time_in_seconds = 0.10;}
 
-                
+                cout<<"formulating the pl-icp"<<endl;
                 //ros::Time start_time = ros::Time::now();
                 for(int i = 0;  i < point_num; ++i){
                     std::vector<int> corr_ind; //index
@@ -289,6 +284,7 @@ void runPl_ICP( Eigen::Quaterniond &pred_q,
                     Eigen::Vector3d cp{current_cloud->points[i].x,current_cloud->points[i].y,current_cloud->points[i].z};
                     Eigen::Vector3d lpa{transformed_cloud->points[corr_ind[0]].x,transformed_cloud->points[corr_ind[0]].y,transformed_cloud->points[corr_ind[0]].z};
                     Eigen::Vector3d lpb{transformed_cloud->points[corr_ind[1]].x,transformed_cloud->points[corr_ind[1]].y,transformed_cloud->points[corr_ind[1]].z};
+                    cout<<"cp: "<<cp[2]<<" lpa: "<<lpa[2]<<" lpb: "<<lpb[2]<<endl;
                     ceres::CostFunction *factor_plicp = Pl_ICP::Create(
                         cp,
                         lpa,
@@ -297,7 +293,7 @@ void runPl_ICP( Eigen::Quaterniond &pred_q,
 
                     problem_.AddResidualBlock(
                         factor_plicp,
-                        config_.loss_function_ptr,
+                        nullptr,//config_.loss_function_ptr,
                         param_.q, param_.t
                     );
         
@@ -308,14 +304,15 @@ void runPl_ICP( Eigen::Quaterniond &pred_q,
 
                 ceres::Solve(config_.options, &problem_, &summary);
                 std::cout << " solve q , t completed " << std::endl;
-                
+                std::cout << summary.FullReport() << "\n";
+
                 Eigen::Quaterniond q{param_.q[3],param_.q[0],param_.q[1],param_.q[2]};
                 Eigen::Vector3d t{param_.t[0],param_.t[1],param_.t[2]};
                 q.normalize();
                 std::cout  << " q.w() : " << q.w()<< " q.x() : " << q.x()<< " q.y() : " << q.y()<< " q.z() : " << q.z()<< std::endl;
                 pred_q = q;
                 pred_t = t;
-                cout<<t[0]<<t[1]<<t[2]<<endl;
+                cout<<"t: "<<t[0]<<" "<<t[1]<<" "<<t[2]<<endl;
 
                 //ros::Time end_time = ros::Time::now();
                 //ros::Duration use_time = end_time - start_time;
@@ -326,45 +323,13 @@ void runPl_ICP( Eigen::Quaterniond &pred_q,
 int main(){
     string path = "/root/bev_fusion/pl_icp/output_demo.json";
     int frmdx;
-    /********************************************************
-     try kdtree
-    ********************************************************/
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);    //创建一个指向点云的指针
-     /**************************生成点云方法1**************************/
-	//初始化点云相关变量
-	cloud->width = 1000;    
-	cloud->height = 1;  //说明是无序点云
-	cloud->points.resize(cloud->width* cloud->height);  //点云的点数
-
-	//生成点云
-	for (std::size_t t = 0; t < cloud->size(); t++)
-	{
-		cloud->points[t].x = 1024.0f * rand() / (RAND_MAX + 1.0f);
-		cloud->points[t].y = 1024.0f * rand() / (RAND_MAX + 1.0f);
-		cloud->points[t].z = 1024.0f * rand() / (RAND_MAX + 1.0f);
-	}
-	/****************************生成点云方法2*************************/
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_1(new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointXYZ point;
-	for (std::size_t t = 0; t < 1000; t++)
-	{
-		point.x = 1024.0f * rand() / (RAND_MAX + 1.0f);
-		point.y = 1024.0f * rand() / (RAND_MAX + 1.0f);
-		point.z = 1024.0f * rand() / (RAND_MAX + 1.0f);
-		cloud_1->push_back(point);       //采用push_back添加点进而生成点云
-	}
-	//创建KD-Tree实例
-	pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
-	kdtree.setInputCloud(cloud);
-    cout<<"set input cloud can work"<<endl;
-	//随机初始化一个查找点
-	pcl::PointXYZ searchPoint; 
-    /********************************************************
-     try kdtree
-    ********************************************************/
+    
+    pcl::visualization::PCLVisualizer::Ptr visualizer(new pcl::visualization::PCLVisualizer("PointCloud Visualizer"));
+    visualizer->setBackgroundColor(0, 0, 0);
 
     
-    frmdx = 15;
+    //current scan
+    frmdx = 3;
     Eigen::Isometry3d cur_T = Eigen::Isometry3d::Identity();
     pcl::PointCloud<pcl::PointXYZI> cur_entire_cloud;
     pcl::PointCloud<pcl::PointXYZI> cur_ld_cloud;
@@ -373,6 +338,11 @@ int main(){
     cout<<"Load current scan"<<endl;
     loadData(path, frmdx, cur_T, cur_entire_cloud, cur_ld_cloud, cur_pc_cloud, cur_rb_cloud);
     cout<<"current pose"<<"\n"<<cur_T.matrix()<<endl;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cur_entire_cloud_ptr(new pcl::PointCloud<pcl::PointXYZI>);
+    *cur_entire_cloud_ptr += cur_entire_cloud;
+    visualizer->addPointCloud<pcl::PointXYZI>(cur_entire_cloud_ptr,"current cloud");
+    
+
 
     //previous scan
     frmdx = 0;
@@ -387,9 +357,49 @@ int main(){
 
     Eigen::Quaterniond q;
     Eigen::Vector3d t;
-    Eigen::Isometry3d T_pc; //= Eigen::Isometry3d::Identity();
-    T_pc = cur_T*pre_T.inverse();
+    Eigen::Isometry3d T_pc= Eigen::Isometry3d::Identity();
+    T_pc = cur_T.inverse()*pre_T;
     cout<<"T_pc"<<"\n"<<T_pc.matrix()<<endl;
-    runPl_ICP(q, t, T_pc, pre_entire_cloud, cur_entire_cloud);
+    /************************************************************************
+     try pcl icp
+    *************************************************************************/
+    pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr transformedxyzi_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::transformPointCloud(pre_entire_cloud,*transformedxyzi_cloud,(Eigen::Isometry3d::Identity()).matrix());
+    icp.setInputSource(transformedxyzi_cloud);
+    icp.setInputTarget(cur_entire_cloud_ptr);
+    icp.setMaximumIterations(100);
+    icp.setMaxCorrespondenceDistance(MAX_DIS);
+    icp.align(*transformedxyzi_cloud);
+    bool icp_succeeded_or_not = icp.hasConverged();
+    double match_score = icp.getFitnessScore();
+    Eigen::Matrix4d transform_pred = icp.getFinalTransformation().cast<double>();
+    /************************************************************************
+     try pl_icp
+    *************************************************************************/
+    /*runPl_ICP(q, t, T_pc, pre_entire_cloud, cur_entire_cloud);
+
+    Eigen::Isometry3d T_pred = Eigen::Isometry3d::Identity();
+    T_pred.rotate(q);
+    T_pred.pretranslate(t);
+    pcl::PointCloud<pcl::PointXYZI>::Ptr transformedxyzi_cloud(new pcl::PointCloud<pcl::PointXYZI>);*/
+    pcl::transformPointCloud(pre_entire_cloud,*transformedxyzi_cloud,transform_pred);
+    //cout<<"transform: "<<"\n"<<(T_pred*T_pc).matrix()<<endl;
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZI> single_color(transformedxyzi_cloud, 0, 255, 0);
+    visualizer->addPointCloud<pcl::PointXYZI>(transformedxyzi_cloud,single_color,"previous cloud");
+
+    visualizer->setPointCloudRenderingProperties(
+    pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "PointCloud");
+    visualizer->addCoordinateSystem(5.0);
+
+    while (!visualizer->wasStopped()) {
+    visualizer->spinOnce(100);}
+
+
+
+
+
+
+
     return 0;
 }
